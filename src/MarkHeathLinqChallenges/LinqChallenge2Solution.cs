c9ed0d18f329db63eb5d4291d9a4c9bf47046871
Tom Pažourek <tom.pazourek@gmail.com>
2018-07-22 13:51:06 +0200

    Solutions for LINQ challenges #2.

---
+++ src/MarkHeathLinqChallenges/LinqChallenge2Solution.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MarkHeathLinqChallenges
+{
+    /// <summary>
+    /// https://markheath.net/post/lunchtime-linq-challenge-2
+    /// </summary>
+    public static class LinqChallenge2Solution
+    {
+        #region Problem 1
+
+        public static int SolveProblem1(string input)
+        {
+            var output = input.Split(',')
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Skip(3)
+                .Sum();
+
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 2
+
+        public static IEnumerable<string> SolveProblem2(string input)
+        {
+            string FormatPosition((int horizontal, int vertical) x)
+                => new string(new [] { (char) (x.horizontal + 'a'), (char) (x.vertical + '1') });
+
+            (int horizontal, int vertical) ParsePosition()
+                => (horizontal: input[0] - 'a', vertical: input[1] - '1');
+
+            var bishopMoveVectors = new[]
+            {
+                (horizontal: +1, vertical: +1),
+                (horizontal: -1, vertical: +1),
+                (horizontal: +1, vertical: -1),
+                (horizontal: -1, vertical: -1),
+            };
+            
+            IEnumerable<(int horizontal, int vertical)> GenerateMoves((int horizontal, int vertical) source, (int horizontal, int vertical) moveVector)
+            {
+                var current = source;
+                while (true)
+                {
+                    var newHorizontal = current.horizontal + moveVector.horizontal;
+                    if (newHorizontal < 0 || newHorizontal > 7)
+                        yield break;
+
+                    var newVertical = current.vertical + moveVector.vertical;
+                    if (newVertical < 0 || newVertical > 7)
+                        yield break;
+
+                    yield return current = (horizontal: newHorizontal, vertical: newVertical);
+                }
+            }
+
+            var inputParsed = ParsePosition();
+            var moves = bishopMoveVectors.SelectMany(x => GenerateMoves(inputParsed, x));
+            var outputFormatted = moves.Select(FormatPosition);
+            return outputFormatted;
+        }
+
+        #endregion
+
+        #region Problem 3
+
+        public static IEnumerable<string> SolveProblem3(string input)
+        {
+            var output = input.Split(',')
+                .Select((x, i) => (item: x, index: i))
+                .Where(x => x.index % 5 == 4)
+                .Select(x => x.item);
+
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 4
+
+        public static int SolveProblem4(string input)
+        {
+            return input.Split(',').Aggregate(0, (result, x) => x == "Yes" ? result + 1 : result - 1);
+        }
+
+        #endregion
+        
+        #region Problem 5
+
+        public static IEnumerable<Problem5Count> SolveProblem5(string input)
+        {
+            var output = input.Split(',')
+                .GroupBy(x => x == "Dog" || x == "Cat" ? x : "Other")
+                .Select(x => new Problem5Count(x.Key, x.Count()));
+
+            return output;
+        }
+
+        public class Problem5Count
+        {
+            public Problem5Count(string animal, int count)
+            {
+                Animal = animal;
+                Count = count;
+            }
+
+            public string Animal { get; }
+            public int Count { get; }
+
+            protected bool Equals(Problem5Count other) => string.Equals(Animal, other.Animal) && Count == other.Count;
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != GetType()) return false;
+                return Equals((Problem5Count) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((Animal != null ? Animal.GetHashCode() : 0) * 397) ^ Count;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Problem 6
+
+        public static string SolveProblem6(string input)
+        {
+            var output = Regex.Replace(input, @"(?<Letter>[A-Z])(?<Count>[0-9]*)", match =>
+            {
+                var groupCountValue = match.Groups["Count"].Value;
+                if (groupCountValue == string.Empty)
+                    return match.Value;
+
+                var groupLetterValue = match.Groups["Letter"];
+
+                var letter = groupLetterValue.Value[0];
+                var count = int.Parse(groupCountValue);
+                return new string(letter, count);
+            });
+
+            return output;
+        }
+
+        #endregion
+    }
+}
+
---
+++ tests/MarkHeathLinqChallenges.Tests/LinqChallenge2Tests.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MarkHeathLinqChallenges.Tests
+{
+    public class LinqChallenge2Tests
+    {
+        [Fact]
+        public void Problem1()
+        {
+            const string input = "10,5,0,8,10,1,4,0,10,1";
+            const int expectedOutput = 48;
+            var actualOutput = LinqChallenge2Solution.SolveProblem1(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Problem2()
+        {
+            const string input = "c6";
+            var expectedOutput = new []
+            {
+                "b7",
+                "a8",
+
+                "d5",
+                "e4",
+                "f3",
+                "g2",
+                "h1",
+
+                "d7",
+                "e8",
+
+                "b5",
+                "a4",
+            };
+            var actualOutput = LinqChallenge2Solution.SolveProblem2(input);
+
+            Assert.Equal(expectedOutput.OrderBy(x => x), actualOutput.OrderBy(x => x));
+        }
+
+        [Fact]
+        public void Problem3()
+        {
+            const string input = "0,6,12,18,24,30,36,42,48,53,58,63,68,72,77,80,84,87,90,92,95,96,98,99,99,100,99,99,98,96,95,92,90,87,84,80,77,72,68,63,58,53,48,42,36,30,24,18,12,6,0,-6,-12,-18,-24,-30,-36,-42,-48,-53,-58,-63,-68,-72,-77,-80,-84,-87,-90,-92,-95,-96,-98,-99,-99,-100,-99,-99,-98,-96,-95,-92,-90,-87,-84,-80,-77,-72,-68,-63,-58,-53,-48,-42,-36,-30,-24,-18,-12,-6";
+            var expectedOutput = new[] {
+                "24",
+                "53",
+                "77",
+                "92",
+                "99",
+                "96",
+                "84",
+                "63",
+                "36",
+                "6",
+                "-24",
+                "-53",
+                "-77",
+                "-92",
+                "-99",
+                "-96",
+                "-84",
+                "-63",
+                "-36",
+                "-6",
+            };
+
+            var actualOutput = LinqChallenge2Solution.SolveProblem3(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Problem4()
+        {
+            const string input = "Yes,Yes,No,Yes,No,Yes,No,No,No,Yes,Yes,Yes,Yes,No,Yes,No,No,Yes,Yes";
+            const int expectedOutput = 3;
+            var actualOutput = LinqChallenge2Solution.SolveProblem4(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Problem5()
+        {
+            const string input = "Dog,Cat,Rabbit,Dog,Dog,Lizard,Cat,Cat,Dog,Rabbit,Guinea Pig,Dog";
+            var expectedOutput = new []
+            {
+                new LinqChallenge2Solution.Problem5Count("Dog", 5),
+                new LinqChallenge2Solution.Problem5Count("Cat", 3),
+                new LinqChallenge2Solution.Problem5Count("Other", 4),
+            };
+            var actualOutput = LinqChallenge2Solution.SolveProblem5(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+        
+        [Fact]
+        public void Problem6()
+        {
+            const string input = "A5B10CD3";
+            const string expectedOutput = "AAAAABBBBBBBBBBCDDD";
+            var actualOutput = LinqChallenge2Solution.SolveProblem6(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
