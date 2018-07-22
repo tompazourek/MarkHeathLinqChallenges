75846c7941563a3678d7a2fa821a540033cd4e17
Tom Pažourek <tom.pazourek@gmail.com>
2018-07-22 12:28:01 +0200

    Initial commit. Solutions for LINQ challenges #1.

---
+++ src/MarkHeathLinqChallenges/LinqChallenge1Solution.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace MarkHeathLinqChallenges
+{
+    /// <summary>
+    /// https://markheath.net/post/lunchtime-linq-challenge
+    /// </summary>
+    public static class LinqChallenge1Solution
+    {
+        #region Problem 1
+
+        public static string SolveProblem1(string input)
+        {
+            var inputItems = input.Split(new [] { ", " }, StringSplitOptions.None);
+            var outputItems = inputItems.Select((x, i) => $"{i + 1}. {x}");
+            var output = string.Join(", ", outputItems);
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 2
+
+        public static IEnumerable<Problem2Player> SolveProblem2(string input)
+        {
+            var inputItems = input.Split(new[] { "; " }, StringSplitOptions.None);
+
+            var outputItems = inputItems
+                .Select(x => x.Split(new[] { ", " }, StringSplitOptions.None))
+                .Select(x => new Problem2Player(x[0], DateTime.ParseExact(x[1], "dd/MM/yyyy", CultureInfo.InvariantCulture)));
+
+            var output = outputItems.OrderByDescending(x => x.DateOfBirth);
+            return output;
+        }
+
+        public class Problem2Player
+        {
+            public Problem2Player(string name, DateTime dateOfBirth)
+            {
+                Name = name;
+                DateOfBirth = dateOfBirth;
+            }
+
+            public string Name { get; }
+            public DateTime DateOfBirth { get; }
+
+            public int GetAge(DateTime now)
+            {
+                var yearDifference = now.Year - DateOfBirth.Year;
+                if (now.Month > DateOfBirth.Month)
+                    return yearDifference;
+
+                if (now.Month == DateOfBirth.Month && now.Day >= DateOfBirth.Day)
+                    return yearDifference;
+
+                return yearDifference - 1;
+            }
+
+            protected bool Equals(Problem2Player other) => string.Equals(Name, other.Name) && DateOfBirth.Equals(other.DateOfBirth);
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != GetType()) return false;
+                return Equals((Problem2Player) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ DateOfBirth.GetHashCode();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Problem 3
+
+        public static TimeSpan SolveProblem3(string input)
+        {
+            var inputItems = input.Split(',');
+            var inputTimeSpans = inputItems.Select(x => TimeSpan.ParseExact(x, @"m\:ss", CultureInfo.InvariantCulture));
+            var output = inputTimeSpans.Aggregate(TimeSpan.Zero, (item, result) => result + item);
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 4
+
+        public static IEnumerable<string> SolveProblem4(int inputX, int inputY)
+        {
+            var sequenceX = Enumerable.Range(0, inputX);
+            var sequenceY = Enumerable.Range(0, inputY);
+
+            var output = sequenceX.SelectMany(x => sequenceY.Select(y => $"{x},{y}"));
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 5
+
+        public static IEnumerable<TimeSpan> SolveProblem5(string input)
+        {
+            var inputItems = input.Split(',');
+            var output = inputItems.Select(x => TimeSpan.ParseExact(x, @"mm\:ss", CultureInfo.InvariantCulture));
+            return output;
+        }
+
+        #endregion
+
+        #region Problem 6
+
+        public static IEnumerable<int> SolveProblem6(string input)
+        {
+            var output = input.Split(',')
+                .Select(x => x.Split('-').Select(int.Parse).ToArray())
+                .SelectMany(x => x.Length == 1
+                    ? new[] { x[0] }
+                    : Enumerable.Range(x[0], x[1] - x[0] + 1));
+
+            return output;
+        }
+
+        #endregion
+    }
+}
+
---
+++ tests/MarkHeathLinqChallenges.Tests/LinqChallenge1Tests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MarkHeathLinqChallenges.Tests
+{
+    public class LinqChallenge1Tests
+    {
+        [Fact]
+        public void Problem1()
+        {
+            const string input = "Davis, Clyne, Fonte, Hooiveld, Shaw, Davis, Schneiderlin, Cork, Lallana, Rodriguez, Lambert";
+            const string expectedOutput = "1. Davis, 2. Clyne, 3. Fonte, 4. Hooiveld, 5. Shaw, 6. Davis, 7. Schneiderlin, 8. Cork, 9. Lallana, 10. Rodriguez, 11. Lambert";
+            var actualOutput = LinqChallenge1Solution.SolveProblem1(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Problem2()
+        {
+            const string input = "Jason Puncheon, 26/06/1986; Jos Hooiveld, 22/04/1983; Kelvin Davis, 29/09/1976; Luke Shaw, 12/07/1995; Gaston Ramirez, 02/12/1990; Adam Lallana, 10/05/1988";
+            var now = new DateTime(2018, 07, 22);
+
+            IList<LinqChallenge1Solution.Problem2Player> expectedOutput = new []
+            {
+                new LinqChallenge1Solution.Problem2Player ("Luke Shaw", new DateTime(1995, 07, 12)),
+                new LinqChallenge1Solution.Problem2Player ("Gaston Ramirez", new DateTime(1990, 12, 02)),
+                new LinqChallenge1Solution.Problem2Player ("Adam Lallana", new DateTime(1988, 05, 10)),
+                new LinqChallenge1Solution.Problem2Player ("Jason Puncheon", new DateTime(1986, 06, 26)),
+                new LinqChallenge1Solution.Problem2Player ("Jos Hooiveld", new DateTime(1983, 04, 22)),
+                new LinqChallenge1Solution.Problem2Player ("Kelvin Davis", new DateTime(1976, 09, 29)),
+            };
+            var expectedAges = new[] { 23, 27, 30, 32, 35, 41 };
+            
+            var actualOutput = LinqChallenge1Solution.SolveProblem2(input).ToList();
+            var actualAges = actualOutput.Select(x => x.GetAge(now)).ToList();
+
+            Assert.Equal(expectedOutput, actualOutput);
+            Assert.Equal(expectedAges, actualAges);
+        }
+
+        [Fact]
+        public void Problem3()
+        {
+            const string input = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
+            var expectedOutput = TimeSpan.FromSeconds(2303);
+
+            var actualOutput = LinqChallenge1Solution.SolveProblem3(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+        
+        [Fact]
+        public void Problem4()
+        {
+            const int inputX = 3;
+            const int inputY = 3;
+            var expectedOutput = new []
+            {
+                "0,0",
+                "0,1",
+                "0,2",
+                "1,0",
+                "1,1",
+                "1,2",
+                "2,0",
+                "2,1",
+                "2,2",
+            };
+
+            var actualOutput = LinqChallenge1Solution.SolveProblem4(inputX, inputY);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+        
+        [Fact]
+        public void Problem5()
+        {
+            const string input = "00:45,01:32,02:18,03:01,03:44,04:31,05:19,06:01,06:47,07:35";
+            var expectedOutput = new []
+            {
+                TimeSpan.FromSeconds(45),
+                TimeSpan.FromMinutes(1) + TimeSpan.FromSeconds(32),
+                TimeSpan.FromMinutes(2) + TimeSpan.FromSeconds(18),
+                TimeSpan.FromMinutes(3) + TimeSpan.FromSeconds(01),
+                TimeSpan.FromMinutes(3) + TimeSpan.FromSeconds(44),
+                TimeSpan.FromMinutes(4) + TimeSpan.FromSeconds(31),
+                TimeSpan.FromMinutes(5) + TimeSpan.FromSeconds(19),
+                TimeSpan.FromMinutes(6) + TimeSpan.FromSeconds(01),
+                TimeSpan.FromMinutes(6) + TimeSpan.FromSeconds(47),
+                TimeSpan.FromMinutes(7) + TimeSpan.FromSeconds(35),
+            };
+
+            var actualOutput = LinqChallenge1Solution.SolveProblem5(input);
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+        
+        [Fact]
+        public void Problem6()
+        {
+            const string input = "2,5,7-10,11,17-18";
+            var expectedOutput = new [] { 2, 5, 7, 8, 9, 10, 11, 17, 18 }.ToList();
+
+            var actualOutput = LinqChallenge1Solution.SolveProblem6(input).ToList();
+
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
